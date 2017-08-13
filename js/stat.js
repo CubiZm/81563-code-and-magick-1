f96b8812f5889f91ac426efa2bb41415d4f328b7
CubiZm <seo-xcube@yandex.ru>
2017-08-13 21:04:36 +0300

    рефакторит гистограму и переменные

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,16 @@
 'use strict';
 
+var gistogram = {
+  WIDTH: 40,
+  HEIGHT: 190,
+  AXIS_X: 80,
+  AXIS_Y: 220,
+  INDENT: 30,
+  PLAYER_COLOR: 'rgba(255, 0, 0, 1)',
+  TEXT_COLOR: 'rgba(0, 0, 0, 1)',
+  MAX: -1,
+};
+
 window.renderStatistics = function (ctx, names, times) {
 
   // Тень
@@ -7,45 +18,42 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillRect(110, 20, 420, 270);
 
   // Облачко
-  ctx.fillStyle = '#fff';
+  ctx.fillStyle = 'rgba(255, 255, 255, 1)';
   ctx.strokeRect(100, 10, 420, 270);
   ctx.fillRect(100, 10, 420, 270);
 
-  ctx.fillStyle = '#000';
+  ctx.fillStyle = 'rgba(0, 0, 0, 1)';
   ctx.font = '16px PT Mono';
 
   ctx.fillText('Ура, вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
 
-  // Время
-
-  var max = -1;
-
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
+  times.forEach(function (elem) {
+    var time = elem;
+    if (time > gistogram.MAX) {
+      gistogram.MAX = time;
     }
-  }
+  });
 
-  // Гистограмма
+  var step = gistogram.WIDTH / gistogram.MAX;
 
-  var histogramWidth = 40;
-  var histogramHeigth = 190;
-  var step = histogramWidth / (max - 0);
+  var getRandomBlueColor = function () {
+    return 'rgba(0, 76, 255, ' + (Math.random() + 0.1) + ')';
+  };
 
-  var initialX = 80;
-  var initialY = 220;
-  var indent = 30;
 
-  for (var j = 0; j < times.length; j++) {
-    ctx.fillStyle = names[j] === 'Вы' ? 'red' : 'rgba(0, 76, 255, ' + '0' + '.' + ((j + 1) * 3) + ')';
-    ctx.fillRect(initialX * (j + 2), initialY, histogramWidth, (times[j] * -step) * 3);
+  var getStatsBar = function (time, name, j) {
+    ctx.fillStyle = names[j] === 'Вы' ? gistogram.PLAYER_COLOR : getRandomBlueColor();
+    ctx.fillRect(gistogram.AXIS_X * (j + 2), gistogram.AXIS_Y, gistogram.WIDTH, (times[j] * -step) * 3);
 
-    ctx.fillStyle = 'black';
-    ctx.fillText(names[j], initialX * (j + 2), initialY + indent);
+    ctx.fillStyle = gistogram.TEXT_COLOR;
+    ctx.fillText(names[j], gistogram.AXIS_X * (j + 2), gistogram.AXIS_Y + gistogram.INDENT);
 
-    ctx.fillStyle = 'black';
-    ctx.fillText(Math.floor(times[j]), initialX * (j + 2), histogramHeigth - (times[j] * step) * 2.5);
-  }
+    ctx.fillStyle = gistogram.TEXT_COLOR;
+    ctx.fillText(Math.floor(times[j]), gistogram.AXIS_X * (j + 2), gistogram.HEIGHT - (times[j] * step) * 2.5);
+  };
+
+  times.forEach(function (time, j) {
+    getStatsBar(time, names[j], j);
+  });
 };
